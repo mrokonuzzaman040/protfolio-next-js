70c402fca123708a9e22a783543d1c9fc2ba9298
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-30 00:56:35 +0600

    Delete unnecessary page components

--- app/blogs/page.js
+++ app/blogs/page.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../components/Nabbar'
 
 export default function Blogs() {
     return (
         <div>
+            <Navbar />
             This is the Blogs Page
         </div>
     )
--- app/projects/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Projects() {
-    return (
-        <div>
-            This is the Projects Page
-        </div>
-    )
-}
