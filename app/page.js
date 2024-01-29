c52ecb2959b2612648275c4404520be1014430bc
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2024-01-30 00:48:16 +0600

    Add HeroSection and AchievementsSection components

--- app/page.js
+++ app/page.js
@@ -1,17 +1,16 @@
 import Image from "next/image";
 import Navbar from "./components/Nabbar";
 import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import AchievementsSection from "./components/AchievementsSection";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <Navbar />
       <div className="container mt-24 mx-auto px-12 py-4">
-        {/* <HeroSection />
+        <HeroSection />
         <AchievementsSection />
-        <AboutSection />
-        <ProjectsSection />
-        <EmailSection /> */}
       </div>
       <Footer />
     </main>
